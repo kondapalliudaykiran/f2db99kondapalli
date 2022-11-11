80bad90fee3ed7be70604bbe3972874bd317a5e7
UDAY KIRAN KONDAPALLI <udaykiran4046@gmail.com>
2022-11-11 10:21:11 -0600

    updated

--- controllers/truck.js
+++ controllers/truck.js
@@ -3,10 +3,17 @@ var truck = require('../models/truck');
 exports.truck_list = function(req, res) {
  res.send('NOT IMPLEMENTED: truck list');
 };
-// for a specific Costume.
-exports.truck_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: truck detail: ' + req.params.id);
-};
+// for a specific truck. 
+exports.truck_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await truck.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}; 
 // Handle Costume create on POST.
 exports.truck_create_post = function(req, res) {
  res.send('NOT IMPLEMENTED: truck create POST');
@@ -15,10 +22,26 @@ exports.truck_create_post = function(req, res) {
 exports.truck_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: truck delete DELETE ' + req.params.id);
 };
-// Handle Costume update form on PUT.
-exports.truck_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: truck update PUT' + req.params.id);
-};
+// Handle truck update form on PUT. 
+exports.truck_update_put = async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await truck.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.truck_name)  
+               toUpdate.truck_name = req.body.truck_name; 
+        if(req.body.truck_type) toUpdate.truck_type = req.body.truck_type; 
+        if(req.body.truck_size) toUpdate.truck_size = req.body.truck_size; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} 
+failed`); 
+    } 
+}; 
 // VIEWS
 
    // List of all Costumes
